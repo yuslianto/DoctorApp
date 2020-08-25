164e4099c115e6d5dc6970d48e15adc591457458
yuslianto <yuslianto56@gmail.com>
2020-08-25 11:10:30 +0700

    first commit

---
+++ src/config/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCBfWXeu7ZYexZqCQXgtV8K74Peq41HBJM',
+  authDomain: 'my-doktor-01.firebaseapp.com',
+  databaseURL: 'https://my-doktor-01.firebaseio.com',
+  projectId: 'my-doktor-01',
+  storageBucket: 'my-doktor-01.appspot.com',
+  messagingSenderId: '582926290824',
+  appId: '1:582926290824:web:a5ad5f9f980ea3407c743d',
+});
+
+const Fire = firebase;
+
+export default Fire;
